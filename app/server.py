43a50f8311784e17aad5974e8c49dcfc046ee37e
Taina Malave <taina.malave@gmail.com>
2021-03-17 10:39:42 -0400

    Create new login route with hardcoded pass

--- app/server.py
+++ app/server.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/login")
+""" @app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template('login.html') """
+
+
+# new login route that should redirect to search/enter page after login
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'password123':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
